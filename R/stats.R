3924b7ee47e979ae008aff09fe67549b134439ec
Tony Boyles <Anthony@Boyles.cc>
2016-04-28 13:23:17 -0400

    Added rmse()

---
+++ R/stats.R
@@ -0,0 +1,12 @@
+#' Calculate root mean square error
+#'
+#' @param x a single value or vector of observed (reference) values
+#' @param y a single value of vector of the same length as \code{x} of predicted values
+#' @return root meas square error
+#' @examples
+#' rmse(x,y)
+#' @export
+
+rmse <- function(x,y) {
+  sqrt(mean((x - y)^2, na.rm = TRUE))
+}
