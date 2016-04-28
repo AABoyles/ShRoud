85b5c6fa950e6d729ee860d17f25bcada6ba308c
Tony Boyles <Anthony@Boyles.cc>
2016-04-28 13:23:06 -0400

    Added stfu()

---
+++ R/stfu.R
@@ -0,0 +1,22 @@
+#' Shut the File Up
+#'
+#' Given an expression, stfu evaluates and returns the value of the expression, but supressesses output to the console.
+#'
+#' @param expr
+#'
+#' @return whatever expr should return
+#' @export
+#'
+#' @examples
+#' > stfu(print("Foo"))
+#' [1] "Foo"
+#' > stfu(cat("Foo"))
+#' NULL
+stfu <- function(expr){
+  fileloc <- tempfile()
+  sink(fileloc)
+  foo <- eval(expr)
+  sink(NULL)
+  unlink(fileloc)
+  return(foo)
+}
