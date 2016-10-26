784f6c8bcf99986a19263f07cde8a1482131f192
Tony Boyles <Anthony@Boyles.cc>
2016-10-26 16:32:52 -0400

    Hacked a fix for Squares and Hexagons. Needs re-writing, but it works now.

--- R/spatial.R
+++ R/spatial.R
@@ -11,21 +11,20 @@
 #'
 #' @examples
 PointsToPolygons <- function(points, x_offsets, y_offsets, dx = 1, dy = 1){
-  df <- sp::coordinates(points)
-  sp::SpatialPolygons(
-    lapply(1:nrow(df), function(i){
-      sp::Polygons(
-        list(
-          sp::Polygon(
-            list(
-              x = df[i,1] + x_offsets * dx,
-              y = df[i,2] + y_offsets * dy
-            )
-          ), paste0("ID", i)
-        )
-      )
-    }), proj4string = sp::CRS(sp::proj4string(points))
-  )
+  grid = data.frame(coordinates(points))
+  npoly = length(grid$x)
+	ret = lapply(1:npoly, function(i){
+	  list(
+	    x = grid$x[i] + x_offsets,
+		  y = grid$y[i] + y_offsets
+	   )
+	})
+	Srl <- vector(mode="list", length=npoly)
+	IDS = paste("ID", 1:npoly, sep="")
+	for (i in 1:npoly)
+		Srl[[i]] = Polygons(list(Polygon(ret[[i]])), IDS[i])
+	res <- SpatialPolygons(Srl, proj4string=CRS(proj4string(points)))
+	res
 }
 
 #' Points to Squares
@@ -39,10 +38,11 @@ PointsToPolygons <- function(points, x_offsets, y_offsets, dx = 1, dy = 1){
 #'
 #' @examples
 PointsToSquares <- function(points){
+  colnames(points@coords) <- c("x", "y")
   d <- min(diff(sort(unique(sp::coordinates(points)[,2]))))/2
-  xs <- c(-1,  1, 1, -1, -1) * d
-  ys <- c(-1, -1, 1,  1, -1) * d
-  PointsToPolygons(points, xs, ys)
+  x_offsets <- c(-1,  1, 1, -1, -1) * d
+  y_offsets <- c(-1, -1, 1,  1, -1) * d
+  PointsToPolygons(points, x_offsets, y_offsets)
 }
 
 #' Points to Hexagons
@@ -58,7 +58,7 @@ PointsToSquares <- function(points){
 PointsToHexagons <- function(points){
   dx <- 2 * min(diff(sort(unique(data.frame(sp::coordinates(points))$x))))
   dy <- dx / sqrt(3)
-  xs <- c(-dx/2,  0, dx/2,  dx/2,   0, -dx/2, -dx/2)
-  ys <- c( dy/2, dy, dy/2. -dy/2, -dy, -dy/2,  dy/2)
-  PointsToPolygons(points, xs, ys)
+  x_offsets <- c(-dx/2,  0, dx/2,  dx/2,   0, -dx/2, -dx/2)
+  y_offsets <- c( dy/2, dy, dy/2, -dy/2, -dy, -dy/2,  dy/2)
+  PointsToPolygons(points, x_offsets, y_offsets)
 }
